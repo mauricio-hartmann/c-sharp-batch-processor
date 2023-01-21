229f9db7453d440b1358e480b65164d601d7875a
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2023-01-21 20:26:26 -0300

    Criado exemplo de execução em batch

---
+++ src/CSBE.Application/BatchProcessor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSBE.Application
+{
+    internal sealed class BatchProcessor
+    {
+        private readonly BlockingCollection<Task> _taskQueue;
+        private readonly CancellationTokenSource _cancellationTokenSource;
+
+        public BatchProcessor(int batchSize)
+        {
+            _taskQueue = new BlockingCollection<Task>(batchSize);
+            _cancellationTokenSource = new CancellationTokenSource();
+        }
+
+        public void Start() => Task.Run(() => ProcessTasksAsync(_cancellationTokenSource.Token));
+
+        public void Stop() => _cancellationTokenSource.Cancel();
+
+        public void EnqueuTask(Task task) => _taskQueue.Add(task);
+
+        private async Task ProcessTasksAsync(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var batch = _taskQueue.GetConsumingEnumerable(cancellationToken);
+                await Task.WhenAll(batch);
+            }
+        }
+    }
+}
---
+++ src/CSBE.Application/Program.cs
@@ -0,0 +1,26 @@
+﻿using CSBE.Application;
+using System;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var processor = new BatchProcessor(30);
+        processor.Start();
+
+        for (int i = 0; i <= 50; i++)
+        {
+            var task = Task.Run(async () => await PrintAsync(i + 1));
+            processor.EnqueuTask(task);
+        }
+
+        processor.Stop();
+    }
+
+    public static async Task PrintAsync(int value)
+    {
+        await Task.Delay(2);
+        Console.WriteLine($"Task: {value}");
+    }
+}
